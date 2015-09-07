1b2dd4155f460684bf53127a3d537221fe7c4503
Elaine Huynh <elainehuynh@Elaines-MacBook-Pro.local>
2015-09-06 22:18:43 -0400

    Initial Commit

---
+++ Assignment1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Assignment1
+//
+//  Created by Elaine Huynh on 9/6/15.
+//  Copyright (c) 2015 edu.cs2048. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Assignment1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Assignment1
+//
+//  Created by Elaine Huynh on 9/6/15.
+//  Copyright (c) 2015 edu.cs2048. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
